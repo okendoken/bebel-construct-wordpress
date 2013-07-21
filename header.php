2dea97eea8534df403ceaa51136d1fa77132fe1c
okendoken <philip.daineka@gmail.com>
2013-07-21 17:25:30 +0300

    footer, header basic

--- header.php
+++ header.php
@@ -1,33 +1,33 @@
-<!doctype html>
+<!DOCTYPE html>
 <html <?php if(isset($doctype)){ language_attributes( $doctype ); } ?>>
 <head>
-<meta charset="utf-8">
-<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<link rel="shortcut icon" type="image/x-icon" href="<?php echo CircleLaw_option('custom-favicon'); ?>" />
-<link rel="icon" type="image/x-icon" href="<?php echo CircleLaw_option('custom-favicon'); ?>" />
-<?php
-if ( is_singular() && get_option( 'thread_comments' ) )
-wp_enqueue_script( 'comment-reply' );
+    <meta charset="utf-8">
+    <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo CircleLaw_option('custom-favicon'); ?>" />
+    <link rel="icon" type="image/x-icon" href="<?php echo CircleLaw_option('custom-favicon'); ?>" />
+    <?php
+        if ( is_singular() && get_option( 'thread_comments' ) )
+            wp_enqueue_script( 'comment-reply' );
 
-wp_head();
-?>
+        wp_head();
+    ?>
 </head>
 <body <?php if (isset($class)){ body_class($class); } ?>>
 
-<div class="container">
+<div class="wrapper container">
   <!--Start Header-->
   <header>
-    <div class="logo">
-      <a href="<?php echo home_url(); ?>"><img src="<?php echo CircleLaw_option('site-logo'); ?>" alt="<?php bloginfo('name'); ?>" /></a>
-    </div><!--End Logo-->
-    <div id="nav">
-        <?php
-        if (!wp_nav_menu( array( 'theme_location' => 'header-menu' ) )){
-          wp_link_pages();
-        }
-        ?>
-    </div><!--End Navigation-->
+      <div class="logo home">
+          <div class="shadow"></div>
+          <a class="logo-content" href="<?php echo home_url(); ?>">
+              <!-- todo LOGO image -->
+              <span class="square"></span>
+              <span class="name">Construct</span>
+              <span class="slogan">We Build Things</span>
+          </a>
+<!--          <a href="--><?php //echo home_url(); ?><!--"><img src="--><?php //echo CircleLaw_option('site-logo'); ?><!--" alt="--><?php //bloginfo('name'); ?><!--" /></a>-->
+      </div><!--End Logo-->
   </header><!--End Header-->
   <!--Start Main Content-->
-  <div class="contenttt">
+  <div class="content">
--- index.php
+++ index.php
@@ -2,42 +2,7 @@
 
 <!-- HomePage -->
 <div id="homepage">
-  
-  <ul id="circles">
-      <?php
-      $per_page = CircleLaw_option('number_home');
-      $paged = get_query_var('paged') ? get_query_var('paged') : 1;
-      $wpbp = new WP_Query(array( 'meta_key' => 'item_order', 'orderby' => 'meta_value_num', 'order' => 'ASC', 'post_type' => 'homepage', 'posts_per_page' => CircleLaw_option('number_home') ) );
-      if ($wpbp->have_posts()) : while ($wpbp->have_posts()) : $wpbp->the_post();
-      $large_image =  wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'fullsize', false, '' ); 
-      $large_image = $large_image[0];
-      ?>
 
-  <!-- Cicles -->
-  <li class="circle">
-    <span class="shadow"></span>
-    <a href="<?php echo get_post_meta($post->ID, 'item_url', true); ?>" class="c-img">
-      <span class="c-imgg"><img src="<?php img_resize($large_image,305,305) ?>" alt="<?php the_title() ?>" title="<?php the_title() ?>"></span></a>
-    <div class="c-details">
-      <div class="c-content">
-        <h2><?php the_title() ?></h2>
-        <div class="det"><?php echo get_post_meta($post->ID, 'item_description', true); ?></div>
-      </div>
-    <a href="<?php echo get_post_meta($post->ID, 'item_url', true); ?>"></a>
-    </div>
-    <div class="clear"></div>
-  </li>
-  <!-- Cicles -->
-
-      <?php endwhile; endif; ?>
-  </ul>
-      <?php if (CircleLaw_option('number_home') > 2){ ?>
-        <div id="pages">
-          <a id="prev2" class="prev" href="#"></a>
-          <a id="next2" class="next" href="#"></a>
-        </div>
-        <div class="clear"></div>
-      <?php } ?>
 </div>
 
 <?php get_footer(); ?>
