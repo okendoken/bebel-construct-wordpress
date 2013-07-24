d515c8de14a874dc234a96ac41b4d826b885eb0e
okendoken <philip.daineka@gmail.com>
2013-07-24 16:06:28 +0300

    slider links

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,10 @@
 <!--Side Bar-->
 <aside class="sidebar">
+    <div class="nav-links">
+        <a class="left" href="#home-carousel" data-slide="prev"><i class="icon-chevron-left"></i></a>
+        <span class="divider"></span>
+        <a class="right" href="#home-carousel" data-slide="next"><i class="icon-chevron-right"></i></a>
+    </div>
     <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
     <?php endif; // end sidebar widget area ?>
 </aside>
