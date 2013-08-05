318616414477d0c0be05b3847533132448a57a91
okendoken <philip.daineka@gmail.com>
2013-08-05 09:38:55 +0300

    mailchimp widget back end tmp

--- bebel/bundles/bebelMailchimpBundle/widgets/NewsletterWidget.class.php
+++ bebel/bundles/bebelMailchimpBundle/widgets/NewsletterWidget.class.php
@@ -51,6 +51,7 @@ class NewsletterWidget extends WP_Widget {
         $instance = $old_instance;
         $instance['title'] = strip_tags($new_instance['title']);
         $instance['description'] = strip_tags($new_instance['description']);
+        $instance['list'] = strip_tags($new_instance['list']);
         $this->flush_widget_cache();
 
         $alloptions = wp_cache_get( 'alloptions', 'options' );
@@ -73,7 +74,32 @@ class NewsletterWidget extends WP_Widget {
 
         <p><label for="<?php echo $this->get_field_id( 'description' ); ?>"><?php _e( 'Description:' ); ?></label>
             <input class="widefat" id="<?php echo $this->get_field_id( 'description' ); ?>" name="<?php echo $this->get_field_name( 'description' ); ?>" type="text" value="<?php echo $description; ?>" /></p>
-    <?php
+        <?php
+        $settings = BebelSingleton::getInstance('BebelSettings');
+        $api_key = $settings->get('mailchimp_apikey');
+        $show_lists = false;
+        $list = isset( $instance['list'] ) ? esc_attr( $instance['list'] ) : '';
+
+        if($api_key == '') {
+            echo '<span style="color: #ff0000"><b>Error:</b> You have to insert a valid api key!</span>';
+        }else {
+            $mcapi = new BebelMailchimp($api_key);
+            if($mcapi->check() == "invalid") {
+                echo '<span style="color: #ff0000"><b>Error:</b> You have to insert a valid api key!</span>';
+            }else {
+                $lists = $mcapi->getLists();
+                $lists = BebelMailchimpUtils::createListforList($lists, $list);
+                $show_lists = true;
+            }
+        }
+        if($show_lists):
+            ?>
+            <label for="<?php echo $this->get_field_id('mailchimp_list') ?>"><?php echo __('Mailchimp List', $settings->getPrefix()) ?></label>
+            <select id="<?php echo $this->get_field_id('mailchimp_list') ?>" name="<?php echo $this->get_field_name('mailchimp_list') ?>">
+                <?php echo $lists; ?>
+            </select>
+            <p class="help">Displays a newsletter signup form that directly sends the email addresses to mailchimp.</p>
+        <?php endif;
     }
 }
 // End Newsletter widget
