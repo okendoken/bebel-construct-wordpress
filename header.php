9a742cc6214a8b1bf8ce23819d94a71c1a61327f
okendoken <philip.daineka@gmail.com>
2013-08-11 13:15:01 +0300

    header, style.css

--- header.php
+++ header.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html <?php language_attributes( ); ?>>
 <head>
-    <meta charset="utf-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
     <title><?php BebelUtils::getPageTitle(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="shortcut icon" type="image/x-icon" href="http://localhost/projects/wordpress/clean/wp-content/themes/Construct/favicon.ico" />
     <link rel="icon" type="image/x-icon" href="http://localhost/projects/wordpress/clean/wp-content/themes/Construct/favicon.ico" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
