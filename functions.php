353583a6f25db25fd10ca6aa07e85a4cde61848d
okendoken <philip.daineka@gmail.com>
2013-10-20 14:17:38 +0300

    docs update

--- functions.php
+++ functions.php
@@ -83,4 +83,5 @@ require_once( get_template_directory().'/install_plugins.php');
 require_once( get_template_directory().'/bebel/core/vendor/mobble/mobble.php');
 require_once( get_template_directory().'/ajax.php');
 require_once( get_template_directory().'/options.php');
+require_once( get_template_directory().'/myFunctions.php');
 // Files Include
