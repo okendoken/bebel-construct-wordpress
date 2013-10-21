fe68417c2ee8c63e03e35a78f3cc16b8f6ab425a
okendoken <philip.daineka@gmail.com>
2013-10-21 15:46:03 +0300

    portfolio fix

--- templates/portfolio.php
+++ templates/portfolio.php
@@ -41,41 +41,16 @@ get_template_part( 'templates/_navigation-no-image', get_post_format() ); ?>
             }
             ?>
             <div class="team" id="portfolio">
-                <?php
-                $i = 0;
-                $row_closed = true;
-                while (have_posts()) {
-                    the_post();
-                    if ($i % 4 == 0){//for each fourth display .row-fluid?>
-                        <div class="row">
-                        <?php
-                        $row_closed = false;
+                <div class="row">
+                    <?php
+                    while (have_posts()) {
+                        the_post();
+                        get_template_part( 'templates/_portfolio-item', get_post_format() );
                     }
-
-                    get_template_part( 'templates/_portfolio-item', get_post_format() );
-
-                    if ($i % 4 == 3){//for each fourth close .row-fluid?>
-                        </div>
-                        <?php
-                        $row_closed = true;
-                    }
-                    $i++;
-                }
-                if (!$row_closed){// if some row went unnoticed
-                    echo '</div>';
-                }
-                ?>
-            </div>
-            <?php
-            global $wp_query;
-            if ($wp_query->max_num_pages > 1){?>
-                <div class="text-align-center">
-                    <ul class="pagination">
-                        <?php echo bebelUtils::getNumberedPagination($wp_query->max_num_pages, $paged, 3, '', 'active'); ?>
-                    </ul>
+                    ?>
                 </div>
-            <?php
-            }
+            </div>
+        <?php
         }
         wp_reset_query();
         wp_reset_postdata();
